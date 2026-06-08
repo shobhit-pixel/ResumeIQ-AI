12a2492a70b52d97b4aa41a1c2346a6f7c7c6a36
shobhit-pixel <formailshobhit@gmail.com>
2026-06-08 08:59:55 +0530

    backend app use updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const uploadRoutes = require("./routes/uploadRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use("/api/upload", uploadRoutes);
